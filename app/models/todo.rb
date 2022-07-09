8e3f75af9abc7e239194b030965ae7c14f709e20
DipeshRaval <dipeshraval50@gmail.com>
2022-07-09 14:57:14 +0530

    Render the list of todos in /todos

---
+++ app/models/todo.rb
@@ -0,0 +1,6 @@
+class Todo < ActiveRecord::Base
+  def to_pleasant_string
+    is_completed = completed ? "[x]" : "[ ]"
+    "#{id}. #{is_completed} #{todo_text} #{due_date.to_fs(:long)}"
+  end
+end
