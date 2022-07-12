e699d78747d5f8ebb5d09a5b0c3f5def59f0dab1
DipeshRaval <dipeshraval50@gmail.com>
2022-07-12 13:56:51 +0530

    Using the ERB for render the html and css also ruby on pages

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,24 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[x]" : "[ ]"
     "#{id}. #{is_completed} #{todo_text} #{due_date.to_fs(:long)}"
   end
+
+  def self.overdue
+    where("due_date < ? ", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ? ", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ? ", Date.today)
+  end
+
+  def self.completed
+    where(completed: true)
+  end
+
+  def self.not_completed
+    where(completed: false)
+  end
 end
