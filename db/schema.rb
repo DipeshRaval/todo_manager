f7e613213c434439233b03b9a8d3b54e32fc96d6
DipeshRaval <dipeshraval50@gmail.com>
2022-07-13 16:15:47 +0530

    Added default route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_10_061210) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_104401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_10_061210) do
     t.text "todo_text"
     t.date "due_date"
     t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
